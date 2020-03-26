9da9b0fa0e351b211fb677f46292dfeddee7378d
fcruzangel <fcruzangel@DML-05589.digitalmanagement.net>
2020-03-26 15:36:47 -0400

    Initial Version

---
+++ sfg-id/src/test/java/guru/springframework/sfgid/SfgIdApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
